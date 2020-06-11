fcf1e402240d50e059936170ee6273bfc807a1b5
Javier Cruz Santiago <javier.cruzs@bancoazteca.com>
2020-06-11 10:49:43 -0500

    Initial Commit

---
+++ AppStoreTransition/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppStoreTransition
+//
+//  Created by Javier Cruz Santiago on 11/06/20.
+//  Copyright © 2020 JCS Development. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
